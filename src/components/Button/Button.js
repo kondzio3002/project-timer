6292ecb246f51028b5f00ec25520bddf4c17dd86
Konrad Brożyna <kondzio3002@gmail.com>
2022-11-19 23:21:57 +0100

    Finish project

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from './Button.module.scss';
 
 const Button = props => (
-  <button className={styles.button}>{props.children}</button>
+  <button className={styles.button} onClick={props.onClick}>{props.children}</button>
 );
 
 export default Button;
